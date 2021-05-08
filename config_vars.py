6205873f97e68f688a82ae6928c4d3f09834d012
Jaakko Kuurne <jaakko.kuurne@gmail.com>
2021-05-08 22:58:13 +0300

    Remove obsolete breakpoint.

--- config_vars.py
+++ config_vars.py
@@ -24,7 +24,6 @@ def set_heroku_vars(token_name='EARTHENGINE_TOKEN'):
                 # else:
                 #     token = content.split(':')[1][2:-2]
                 secret = '{}={}'.format(token_name, token)
-                breakpoint()
                 if platform.system() == 'Windows':
                     check_call(['heroku', 'config:set', secret], stdout=DEVNULL, stderr=STDOUT, shell=True)
                 else:
